cdda64937a86991e4bb6410f4f6d0bfd131b3c86
Vivek Chavan <vivek.c1994@gmail.com>
2023-11-27 17:16:03 +0400

    Logo Update

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -4,9 +4,13 @@ export function DefaultLayout(props) {
     return (
         <div>
             <div className="header bs1">
+                
                 <div className="d-flex justify-content-between">
-                    <h2>EY Vehicle Rental</h2>
-               
+                <img height="100px" width="auto" className="" 
+                src="https://images.cdn.centreforaviation.com/stories/companies/airlines/etihad/etihad_partners.jpg"/> 
+                <img height="100px" width="auto" className="" 
+                src="https://static.vecteezy.com/system/resources/thumbnails/008/461/390/small/car-rental-logo-on-white-background-luxury-car-illustration-modern-business-concept-car-rental-company-transportation-service-business-vector.jpg"/> 
+                
                 </div>
             </div>
             <div className="content">
