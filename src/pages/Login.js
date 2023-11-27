8977c00cd272b2935f5dd3e4b866b4c812081b4e
Vivek Chavan <vivek.c1994@gmail.com>
2023-11-27 16:51:57 +0400

    Login CSS

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Row,Col,Form,Input, Button} from 'antd';
 
 export function Login() {
     return(
-        <div>
-            <h1>Login</h1>
+        <div class='loginForm p-5'>
+            <Row className="pd-5"></Row>
+            <Row gutter={16} className='d-flex align-items-center'>
+                <Col lg={9}></Col>
+                <Col lg={6} className='text-left p-5'>
+                    <h1>Login</h1>
+                    <hr/>
+                    <Form layout='vertical'>
+                        <Form.Item name='username' label='Username'rules={[{ required: true, message: 'Please input your username!' }]}>
+                           <Input/>
+                        </Form.Item>
+                        <Form.Item name='password' type='password' label='Password'rules={[{ required: true, message: 'Please enter a valid password!' }]}>
+                        <Input.Password />
+                        </Form.Item>
+
+                        <button>Login</button>
+                        <hr/>
+                        <Link to='/signup'> Create an Account</Link>
+                    </Form>
+                </Col>
+            </Row>
+         
         </div>
     );
 }
