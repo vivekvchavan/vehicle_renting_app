3e2c9dbec30e90d5987b207229dd50902958300d
Vivek Chavan <vivek.c1994@gmail.com>
2023-11-27 16:05:01 +0400

    Home Page UI Changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Login } from './pages/Login';
 import { CreateAccount } from './pages/CreateAccount';
 import { Booking } from './pages/Booking';
 import './App.css';
+import 'antd/dist/antd';
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,39 @@ import React, { useEffect } from 'react';
 import { DefaultLayout } from '../components/DefaultLayout';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllVehicles } from '../redux/actions/vehicleActions';
-
+import { Button, Row, Col } from 'antd';
 
 export function Home() {
-    const {vehicle} = useSelector(state=>state.vehicleReducer);
-    const {loading} = useSelector(state=>state.alertsReducer);
-    
+    const { vehicle } = useSelector(state => state.vehicleReducer);
+    const { loading } = useSelector(state => state.alertsReducer);
+
     const dispatch = useDispatch();
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getAllVehicles())
     }, []);
 
-    return(
+    return (
         <DefaultLayout>
-           <h1>Home Page</h1>
-           <h1>The length is vehicles is {vehicle.length}</h1>
+            <Row justify='center'>
+                {vehicle.map(vehicle => {
+                    return <Col lg={5} sm={24} xs={24}>
+                        <div className="vehicleDisplay p-3 mt-5">
+                            <img src={vehicle.image} class="vehicleImg" />
+                            <div>
+                                <h4>{vehicle.name}</h4>
+                                <p>{vehicle.rentPerHour} {vehicle.currencyType} / Hr</p>
+                            </div>
+
+                            <div>
+                                <Button>Book</Button>
+                            </div>
+                        </div>
+
+                    </Col>
+                })
+                }
+            </Row>
         </DefaultLayout>
     );
 }
